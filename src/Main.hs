ba8c556977f6cc7546e96d32d3cec822ca20fcc4
Lokathor <zefria@gmail.com>
2015-11-02 22:52:51 -0700

    Can connect to irc.freenode.net and print that it did.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 
 module Main where
 
-main = putStrLn "Foo."
+import Network.Simple.TCP
 
+main :: IO ()
+main = withSocketsDo $ do
+    connect "irc.freenode.net" "6667" $ \(connectionSocket, remoteAddr) -> do
+        putStrLn $ "Connection Established to " ++ show remoteAddr
+        putStrLn "Now Exiting."
