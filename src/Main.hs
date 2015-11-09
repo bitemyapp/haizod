e2c1c44a10badc6004537ccc3b531465dcb8fdd1
Lokathor <zefria@gmail.com>
2015-11-09 16:05:22 -0700

    Updates to get things working with GHC 7.10.3 now.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,20 @@
 
 module Main where
 
+--import Network.Socket.ByteString (send, recv)
 import Network.Simple.TCP
+import Control.Monad (forever)
+import Control.Concurrent.Async
 
 main :: IO ()
 main = withSocketsDo $ do
     connect "irc.freenode.net" "6667" $ \(connectionSocket, remoteAddr) -> do
         putStrLn $ "Connection Established to " ++ show remoteAddr
-        putStrLn "Now Exiting."
+        withAsync (printIncoming connectionSocket) $ \a -> do
+            terminalLine <- getLine
+            putStrLn "Now Exiting."
+
+printIncoming :: Socket -> IO ()
+printIncoming socket = forever $ do
+    msg <- recv socket 512
+    print msg
