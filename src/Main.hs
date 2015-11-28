df9430ea3b1b2c696dd357c8cb9484bb5bd60d36
Lokathor <zefria@gmail.com>
2015-11-27 21:19:22 -0700

    Added small improvements, and stack.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
 --import Network.Socket.ByteString (send, recv)
+import Data.ByteString (putStr)
 import Network.Simple.TCP
 import Control.Monad (forever)
 import Control.Concurrent.Async
@@ -10,11 +12,18 @@ main :: IO ()
 main = withSocketsDo $ do
     connect "irc.freenode.net" "6667" $ \(connectionSocket, remoteAddr) -> do
         putStrLn $ "Connection Established to " ++ show remoteAddr
+        send connectionSocket "NICK haizod \r\n"
+        send connectionSocket "USER haizod 0 * :haizod\r\n"
         withAsync (printIncoming connectionSocket) $ \a -> do
             terminalLine <- getLine
             putStrLn "Now Exiting."
 
 printIncoming :: Socket -> IO ()
-printIncoming socket = forever $ do
+printIncoming socket = do
     msg <- recv socket 512
-    print msg
+    case msg of
+        Just s -> do
+            Data.ByteString.putStr s
+            printIncoming socket
+        Nothing -> do
+            print "No more data, closing."
