5ce10518dcb53611fee079255e7eef9db792bf60
Daniel Gee <zefria@gmail.com>
2015-11-28 17:49:23 -0700

    Successfully does server in one thread while the main thread waits for all servers to close out.

--- src/Main.hs
+++ src/Main.hs
@@ -3,20 +3,44 @@
 module Main where
 
 --import Network.Socket.ByteString (send, recv)
-import Data.ByteString (putStr)
+import Data.ByteString (putStr, isPrefixOf, append, drop)
 import Network.Simple.TCP
 import Control.Monad (forever)
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.Async
+import Control.Concurrent.STM
+import qualified Data.Set as S
+import Data.Set (Set)
+
+data ServerData = ServerData {
+    getServerName :: String,
+    getServerPort :: Int
+    } deriving (Eq, Ord, Show)
+
+serverConnect :: ServerData -> TVar (Set ServerData) -> IO ()
+serverConnect servdat connectionsTVar = connect (getServerName servdat) (show . getServerPort $ servdat) $
+    \(socket, remoteAddr) -> do
+        atomically $ modifyTVar' connectionsTVar (\connections -> S.insert servdat connections)
+        putStrLn $ "Connection Established to " ++ show remoteAddr
+        send socket "NICK haizod\r\n"
+        send socket "USER haizod 0 * :haizod\r\n"
+        printIncoming socket
+        atomically $ modifyTVar' connectionsTVar (\connections -> S.delete servdat connections)
 
 main :: IO ()
 main = withSocketsDo $ do
-    connect "irc.freenode.net" "6667" $ \(connectionSocket, remoteAddr) -> do
-        putStrLn $ "Connection Established to " ++ show remoteAddr
-        send connectionSocket "NICK haizod \r\n"
-        send connectionSocket "USER haizod 0 * :haizod\r\n"
-        withAsync (printIncoming connectionSocket) $ \a -> do
-            terminalLine <- getLine
-            putStrLn "Now Exiting."
+    let servData = ServerData "irc.freenode.net" 6667
+    connectionsTVar <- atomically $ newTVar S.empty
+    withAsync (serverConnect servData connectionsTVar) $ \a -> do
+        threadDelay 1000000 -- We delay to give the first connection time to open before waiting.
+        waitToQuit connectionsTVar
+
+waitToQuit :: TVar (Set ServerData) -> IO ()
+waitToQuit connectionsTVar = atomically $ do
+    connections <- readTVar connectionsTVar
+    if S.null connections
+        then return ()
+        else retry
 
 printIncoming :: Socket -> IO ()
 printIncoming socket = do
@@ -24,6 +48,13 @@ printIncoming socket = do
     case msg of
         Just s -> do
             Data.ByteString.putStr s
-            printIncoming socket
+            if Data.ByteString.isPrefixOf "PING " s
+                then do
+                    --let reply = "PONG " `Data.ByteString.append` (Data.ByteString.drop 5 s)
+                    let reply = "QUIT :exiting.\r\n"
+                    Data.ByteString.putStr (">" `Data.ByteString.append` reply)
+                    send socket reply
+                    return ()
+                else printIncoming socket
         Nothing -> do
-            print "No more data, closing."
+            print "Socket closed."
